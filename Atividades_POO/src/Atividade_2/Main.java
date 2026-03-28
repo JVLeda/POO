b083ec4c3afa77f89f7379f3806dd01e4069fa01
João Vitor Lêda Nogueira <leda@unifesspa.edu.br>
2026-03-28 11:44:18 -0300

    Atividades 1, 2, 3 e MindDebug

---
+++ Atividades_POO/src/Atividade_2/Main.java
@@ -0,0 +1,11 @@
+package Atividade_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Amélia");
+        p1.setIdade(19);
+        p1.mostrarDados();
+    }
+}
